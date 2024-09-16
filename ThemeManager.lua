9f4a3825ca1965d274eb56fed17b104f088ac8df
ipufo1 <116466030+ipufo1@users.noreply.github.com>
2024-09-15 23:04:12 -0700

    Update ThemeManager.lua

--- ThemeManager.lua
+++ ThemeManager.lua
@@ -49,7 +49,7 @@ local ThemeManager = {} do
 	end
 
 	function ThemeManager:LoadDefault()		
-		local theme = 'Default'
+		local theme = 'Tokyo Night'
 		local content = isfile(self.Folder .. '/themes/default.txt') and readfile(self.Folder .. '/themes/default.txt')
 
 		local isDefault = true
@@ -67,7 +67,7 @@ local ThemeManager = {} do
 		if isDefault then
 			Options.ThemeManager_ThemeList:SetValue(theme)
 		else
-			self:ApplyTheme('Tokyo Night')
+			self:ApplyTheme(theme)
 		end
 	end
 
@@ -113,6 +113,12 @@ local ThemeManager = {} do
 			Options.ThemeManager_CustomThemeList:SetValue(nil)
 		end):AddButton('Load theme', function() 
 			self:ApplyTheme(Options.ThemeManager_CustomThemeList.Value) 
+		end)
+
+		groupbox:AddButton('Set as default', function()
+			self:SaveDefault(Options.ThemeManager_CustomThemeList.Value)
+			self.Library:Notify(string.format('Set default custom 
+						theme to %q', Options.ThemeManager_ThemeList.Value))
 		end)
 
 		groupbox:AddButton('Refresh list', function()
